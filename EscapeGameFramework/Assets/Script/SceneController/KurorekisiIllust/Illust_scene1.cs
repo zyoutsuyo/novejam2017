a306c7a80d14f538355222c4482c14174f1ef287
USER <user@USERnoMacBook.local>
2017-07-23 13:31:13 +0900

    Shundo commit

---
+++ EscapeGameFramework/Assets/Script/SceneController/KurorekisiIllust/Illust_scene1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Illust_scene1 : GameScreenEventBase {
+
+	// Use this for initialization
+	void Start () {
+		ExcuteEvent ();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void ExcuteEvent ()
+	{
+			WindwManager.Instance.OpenStoryWindw (
+				GameManager.Instance.MasterStoryData.Where (m => m.id <= 4).ToList ()
+			).SetEndCallBack (() => {
+
+				var c = transform.Find ("Color").GetComponent<Image>();
+				c.color = new Color(1, 0, 0);
+
+				WindwManager.Instance.OpenStoryWindw (
+					GameManager.Instance.MasterStoryData.Where (m => 5 <= m.id && m.id <= 6).ToList ()
+				).SetEndCallBack (() => {
+					StartCoroutine(Blink());
+				});
+			});
+	}
+
+	IEnumerator Blink(){
+		var c = transform.Find ("Color").GetComponent<Image>();
+		for (int i = 0; i < 10; i++) {
+			c.color = new Color (1, 1, 1);
+			yield return new WaitForSeconds (0.05f);
+			c.color = new Color (0,0,0);
+			yield return new WaitForSeconds (0.05f);
+		}
+
+
+		StartCoroutine(StartNext());
+	}
+
+
+	IEnumerator StartNext(){
+		yield return new WaitForSeconds (0.5f);
+		WindwManager.Instance.OpenStoryWindw (
+			GameManager.Instance.MasterStoryData.Where (m => 7 <= m.id && m.id <= 11).ToList ()
+		).SetEndCallBack (() => {
+			// GameManager.Instance.AddPlayerItem (1);
+		});
+	}
+}
---
+++ EscapeGameFramework/Assets/Script/SceneController/KurorekisiIllust/Illust_scene3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Illust_scene3 : GameScreenEventBase {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator Light(){
+		var c = transform.Find ("Color").GetComponent<Image>();
+		for (float f = 0; f <= 1; f += 0.04f) {
+			c.color = new Color (f, f, f);
+			yield return new WaitForSeconds (0.05f);
+		}
+
+		StartNext();
+	}
+
+	void StartNext(){
+		WindwManager.Instance.OpenStoryWindw (
+			GameManager.Instance.MasterStoryData.Where (m => 7 == m.id || (13 <= m.id && m.id <= 14)).ToList ()
+		).SetEndCallBack (() => {
+			// GameManager.Instance.AddPlayerItem (1);
+		});
+	}
+
+	public override void ExcuteEvent ()
+	{
+	}
+}
